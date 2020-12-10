4933a2ef4c7a9dbeeda31ad4cd84920cf77e1c9d
Wallis <wallis@pubbly.com>
2020-12-09 22:20:34 -0500

    added styling, more options, random background, and random audio

--- js/generate.js
+++ js/generate.js
@@ -2,17 +2,51 @@ console.log("js loaded");
 
 
 $(document).ready(function() {
-    console.log("page ready");
-    list = ["good","bad","ugly"];
-    populate(list, "#world");
+    generate();
+
+    $(".button").click(function() {
+        generate();
+        startAudio();
+    });
 })
 
+function generate() {
+    $.getJSON("js/options.json", function(data, generate){
+        $(".field").each(function() {
+            populate(this, data[$(this).attr("id")]);
+        });
+    }).fail(function(){
+        console.log("An error has occurred.");
+    });
 
-function populate(list, div) {
+    genRandomBackground();
+}
+
+
+function populate(div, list) {
+    // div.text(list[getRandomInt(list.length)]);
     $(div).text(list[getRandomInt(list.length)]);
-    
 }
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getRandomColor() {
+    // https://css-tricks.com/snippets/javascript/random-hex-color/
+    return Math.floor(Math.random()*16777215).toString(16);
+}
+
+function genRandomBackground() {
+    let c1 = getRandomColor();
+    let c2 = getRandomColor();
+    $('html').css('background', '-webkit-gradient(linear,left top,left bottom,from(#' + c1 + '),to(#' + c2 + '))');
+}
+
+function startAudio() {
+    audios = ["beach","city","farm","rain","scifi","space","wind","windchimes"];
+    i = getRandomInt(audios.length);
+    // $("#player").src("audios/" + audios[i] + ".ogg");
+    $("#player").attr("src", "audio/" + audios[i] + ".ogg");
+    $("#player").trigger('play');
 }
